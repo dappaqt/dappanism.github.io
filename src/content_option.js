e7545e02656d5f40c560732cd4e82ab56461cf34
dappaqt <gan.viclates32221@gmail.com>
2023-06-01 13:20:12 +0700

    Menambahkan commitf file

---
+++ src/content_option.js
@@ -0,0 +1,90 @@
+const logotext = "JOHN";
+const meta = {
+    title: "John Doe",
+    description: "I’m John Doe data scientist _ Full stack devloper,currently working in Berlin",
+};
+
+const introdata = {
+    title: "HELLO WORLD",
+    animated: {
+        first: "I love coding",
+        second: "WELCOME TO GODS SYNDROME",
+        third: "I develop mobile apps",
+    },
+    description: "GOD IS THE EARLIEST OF ALL THINGS, BECAUSE HE HAS NO BIRTH AND NOTHING IS MORE ACTIVE THAN THE MIND, BECAUSE HE MOVES ABOVE THE UNIVERSE AND THERE IS NOTHING STRONGER THAN A COMPULSION TO SUBMIT TO HIM.",
+    your_img_url: "https://telegra.ph/file/0cdadd1760f915af20aac.png",
+};
+
+const dataabout = {
+    title: "all about gods syndrome",
+    aboutme: "FALLEN ANGEL IN ABRAHAMIC TERMS ARE ANGELS WHO WERE CAST OUT OF HEAVEN. THE TERM FALLEN ANGEL IS NOT FOUND IN THE BIBLE.",
+};
+const SINCE = [{
+        jobtitle: "GODS SYNDROME",
+        where: "SINCE",
+        date: "2019",
+    },
+];
+
+const dataportfolio = [{
+        img: "https://i.ibb.co/vVmcPnV/GODS-SYNDROME19.jpg",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/kMMyWhL/20220523-051246.jpg",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/VLCS4Lv/G0-DS-5-YNDROM3.jpg",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/BsJCygG/Gods-anime.png",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/xXW7x7C/GODS-WALLPAPER.jpg",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/6Z7xZ6X/Godsss.jpg",
+        link: "#",
+    },
+
+    {
+        img: "https://i.ibb.co/ZTrbZGX/HBD-GODS.png",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/mG9YzWj/IMG-20230601-11234066.jpg",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/WszRvXF/Picsart-23-05-29-21-30-12-280.jpg",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/Sx98Kx4/Picsart-23-05-11-07-29-50-878.jpg",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/r65TTRm/Picsart-23-05-23-21-48-50-079.png",
+        link: "#",
+    },
+    {
+        img: "https://i.ibb.co/bFXrXmk/Gods-QRCODE.png",
+        link: "#",
+    },
+];
+export {
+    meta,
+    dataabout,
+    dataportfolio,
+    worktimeline,
+    skills,
+    services,
+    introdata,
+    contactConfig,
+    socialprofils,
+    logotext,
+};
